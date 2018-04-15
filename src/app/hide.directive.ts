d57e9badf0e99ad4a2b4f827372d48fa7541f4e9
Sourabh Ranka <sranka29@rediffmail.com>
2018-04-15 16:17:44 -0700

    more changes

--- src/app/hide.directive.ts
+++ src/app/hide.directive.ts
@@ -5,12 +5,10 @@ import { Directive, ElementRef, Renderer, HostListener, Input } from '@angular/c
 })
 export class HideDirective {
 
-
-  constructor(public el: ElementRef, public rend: Renderer) {
-
-  }
-
   @Input() check: boolean;
+  
+  constructor(public el: ElementRef, public rend: Renderer) {
+  }
 
   ngOnInit() {
     if (this.check === true) {
@@ -19,16 +17,24 @@ export class HideDirective {
 
   }
 
+  ngOnChanges(){
+    if (this.check === true) {
+      this.rend.setElementStyle(this.el.nativeElement, 'display', 'none');
+    }else if(this.check === false){
+      this.rend.setElementStyle(this.el.nativeElement, 'display', 'block');
+    }
+  }
+
   @HostListener('mouseover') onMouseEnter() {
-    this.rend.setElementStyle(this.el.nativeElement, 'backgroundColor', 'green');
+    this.rend.setElementStyle(this.el.nativeElement, 'backgroundColor', 'lightgreen');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-         this.rend.setElementStyle(this.el.nativeElement, 'backgroundColor', 'white');
-    }
+    this.rend.setElementStyle(this.el.nativeElement, 'backgroundColor', 'white');
+  }
 
-  
-   
-  
+
+
+
 
 }
