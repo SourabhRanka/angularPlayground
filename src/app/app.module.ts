93f29b66c183c714c8679a8238cd8bd4b394c3c1
Sourabh Ranka <sranka29@rediffmail.com>
2018-04-28 23:13:28 -0700

    adding feature modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 	import { FormsModule } from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+  import { AppComponent } from './app.component';
+  import { Feature1Module } from "./feature1/feature1.module";
+
 
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,Feature1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
