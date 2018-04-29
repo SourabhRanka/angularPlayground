f904866cc93b4ba35ca760a2276d9ce136af27fa
Sourabh Ranka <sranka29@rediffmail.com>
2018-04-28 23:21:26 -0700

    sharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 	import { FormsModule } from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+  import { AppComponent } from './app.component';
+  import { Feature2Module } from "./feature2/feature2.module";
+  import { Feature1Module } from "./feature1/feature1.module";
 
 
 @NgModule({
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,Feature1Module,Feature2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
